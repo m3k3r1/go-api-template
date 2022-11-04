785ca5f6d3d9bab7040c4dbabab936a2812885f9
m3k3r1 <m3k3r1@gmail.com>
2022-11-05 00:07:42 +0100

    add chi router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/m3k3r1/go-api-template/internal/infra/database"
+	"github.com/m3k3r1/go-api-template/internal/infra/webservers/handlers"
+	"net/http"
 
+	"github.com/m3k3r1/go-api-template/configs"
+	"github.com/m3k3r1/go-api-template/internal/entity"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	config, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":"+config.WebServerPort, r)
 }
